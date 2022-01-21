83024d60f6a334e2e6b50d474b3d10c41b42d501
rohullahN <41601768+rohullahN@users.noreply.github.com>
2022-01-21 03:47:07 -0500

    Implemented all functionality including playing sound

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ var level = 0;
 $(document).keypress(function(){
     if(!gameStarted)
     {
+        $("body").removeClass("gameOver");
         $(".title").text("Level " + level );
         gameStarted=true;
         generateSequence();
@@ -18,6 +19,7 @@ $(document).keypress(function(){
 
 function generateSequence()
 {
+    userSequence=[];
     level++;
     $(".title").text("Level " + level );
     var randColor = Math.floor(Math.random()* btnColors.length);
@@ -25,10 +27,75 @@ function generateSequence()
     console.log(sequence);
 
  toggleButton(btnColors[randColor]);
+ playSound(btnColors[randColor]);
 }
 
 function toggleButton(color)
 {
-    $("."+color).fadeOut(150).fadeIn(150).fadeOut(150).fadeIn(150);
+    $("."+color).fadeOut(150).fadeIn(150);
     
+}
+
+$(".btn").click(function()
+{
+    var clickedColor= $(this).attr("id");
+    var colorIndex;
+    switch(clickedColor)
+    {
+        case "red":
+            colorIndex=0;
+            
+            break;
+        case "green":
+            colorIndex=1;
+            
+            break;
+        case "yellow":
+            colorIndex=2;
+        
+            break;
+        case "blue":
+            colorIndex=3;
+            
+            break;
+        default:
+            colorIndex=9;
+            break;
+    }
+    userSequence.push(colorIndex);
+    console.log(sequence);
+    toggleButton(clickedColor);
+    playSound(clickedColor);
+
+    
+    checkColors();
+
+    
+});
+
+function checkColors()
+{   
+    if(sequence[userSequence.length-1] === userSequence[userSequence.length-1])
+    {
+        if (sequence.length === userSequence.length)
+        {
+            setTimeout(function () {
+                generateSequence();
+              }, 1000); 
+        }
+    }
+    else{
+            playSound("wrong");
+            $("body").addClass("gameOver");
+            $(".title").text("Game Over. Press Any Key to Restart the Game")
+            gameStarted=false;
+            sequence=[];
+            level=0;
+    }
+}
+
+function playSound(color)
+{
+    var audio = new Audio("sounds/"+color+".mp3");
+    audio.play();
 }
